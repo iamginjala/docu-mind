80707de0671f31f4d1b938ef96d6433fd82b26d4
Harsha Vardhan Reddy Ginjala <harshaginjaladec23@gmail.com>
2026-03-19 18:13:07 -0400

    initial commit: working RAG app with FastAPI, LangGraph, pgvector, Streamlit

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI,UploadFile
+from pydantic import BaseModel
+from typing import Annotated,List
+from app.embeddings import generate_embeddings, store_embeddings
+from app.parser import process_document
+from app.rag import app as rag_app
+import os
+
+
+class Question(BaseModel):
+    ques: str
+    chat_history: list = []
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/upload")
+async def upload_documents(files: List[UploadFile]):
+    upload_directory = "documents"
+    os.makedirs(upload_directory,exist_ok=True)
+    for file in files:
+        file_location = f"{upload_directory}/{file.filename}"
+        with open (file_location, "wb") as buffer:
+            buffer.write(await file.read())
+        filename,chunks = process_document(file_location)
+        content,embeddings = generate_embeddings(chunks)
+        store_embeddings(content,embeddings,filename)
+    
+    return {"message": "successfully processed", "files": [file.filename for file in files]}
+
+@app.post("/ask")
+async def llm_response(question: Question):
+    result = rag_app.invoke({
+      "question": question.ques,
+      "chunks": [],
+      "answer": "",
+      "chat_history": question.chat_history
+    })
+    return {"answer": result["answer"],"chat_history": result["chat_history"]}
