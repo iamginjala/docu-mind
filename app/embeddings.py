5c745d6184b4c7890b915008bbf9bed967064a38
Harsha Vardhan Reddy Ginjala <harshaginjaladec23@gmail.com>
2026-03-19 09:31:18 -0400

    completed rag pipeline

--- app/embeddings.py
+++ app/embeddings.py
@@ -27,19 +27,10 @@ def store_embeddings(content,embeddings,filename):
         cursor.execute( "INSERT INTO documents (metadata,contents,embeddings) VALUES (%s, %s, %s)",
                    (filename,content[i],list(embeddings[i].values)))
     conn.commit()
-    cursor.execute("select * from documents;")
-    rows = cursor.fetchall()
-
-    for i in rows:
-        print(i)
     cursor.close()
     conn.close()
 
     return True
-
-# chunks = process_document('documents/scammer-agent.pdf')
-# content,embeddings = generate_embeddings(chunks)
-# print(store_embeddings(content,embeddings,"scammer-agent.pdf"))
 
 def generate_question_embeddings(question):
     result = client.models.embed_content(
@@ -48,23 +39,18 @@ def generate_question_embeddings(question):
         config=types.EmbedContentConfig(output_dimensionality=1536)
     )
     return result.embeddings
+
 def search_similar(question):
     question_embeddings = generate_question_embeddings(question)
     ques_embeddings = list(question_embeddings[0].values)
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute("select metadata,contents,1-(embeddings <=> (%s::vector))  as similarity  "  \
-    "from documents order by embeddings <=> (%s::vector) asc limit 5;",
-    (ques_embeddings,ques_embeddings))
+    query = "select contents from documents where (1-(embeddings <=> (%s::vector))) > 0.3 order by embeddings <=> (%s::vector) asc limit 5;"
+    cursor.execute(query,(ques_embeddings,ques_embeddings))
 
     rows = cursor.fetchall()
-
-    for i in rows:
-        print(i)
-    
     cursor.close()
     conn.close()
 
-    return True
 
-print(search_similar("what do you want to know about grass?"))
+    return [row[0] for row in rows]
