3bb68512832fa32ee1197e58d1707f675c3d3df8
Harsha Vardhan Reddy Ginjala <harshaginjaladec23@gmail.com>
2026-03-19 09:31:46 -0400

    completed rag pipeline

--- app/parser.py
+++ app/parser.py
@@ -1,6 +1,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import pymupdf4llm
-
+import os
 
 def process_document(path):
     """
@@ -8,20 +8,22 @@ def process_document(path):
     Recursive character text splitter
     """
     text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
+    chunk_size=1000,
     chunk_overlap=50,
     length_function=len,
     )
     if path.endswith(".pdf"):
         txt = pymupdf4llm.to_text(path)
+        filename = os.path.basename(path)
         texts = text_splitter.create_documents([txt])
-        return texts
+        return filename,texts
     
     elif path.endswith(".txt"):
+        filename = os.path.basename(path)
         with open(path,encoding="utf-8") as f:
             txt = f.read()
         texts = text_splitter.create_documents([txt])
-        return texts
+        return filename,texts
     
     else:
         raise ValueError("only pdf and text files")
